b1ec23cfc388da358cabea8fd56f6c03db30ca5f
Javier Ramirez <apollo.infinity88@gmail.com>
2019-12-17 10:12:14 -0800

    Added a base.html for basic elements to be shown on every page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Javier'}
+    posts = [
+        {
+            'author': {'username': 'Javier'},
+            'body': 'Beautiful Day in the Central Valley'
+        },
+        {
+            'author': {'username': 'Mochi'},
+            'body': 'Where my money at?!'
+        }
 
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
